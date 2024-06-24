2fdb566ae597af82045f9c74a3550946c08f4276
Josip Hudolin <josip.hudolin315@gmail.com>
2024-06-25 00:33:01 +0200

    mala izmjena kompasa

--- app/src/main/java/com/example/trailquest/CompassScreen.kt
+++ app/src/main/java/com/example/trailquest/CompassScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CompassScreen(onBackClick: () -> Unit) {
